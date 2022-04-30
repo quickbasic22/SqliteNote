397401033770ff1141ba9669215b873d76be8ceb
David Morrow <quickbasic44@outlook.com>
2022-04-30 19:31:12 -0400

    Add project files.

---
+++ SqliteNote/SqliteNote/Views/ItemsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SqliteNote.Models;
+using SqliteNote.ViewModels;
+using SqliteNote.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SqliteNote.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+
+        private void SwipeItem_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Just a test", "Hello, World!", "Cancel");
+
+        }
+    }
+}
---
+++ SqliteNote/SqliteNote/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SqliteNote.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SqliteNote.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
